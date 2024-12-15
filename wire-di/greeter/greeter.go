e4230a374502e322b8f61c1a1c5e37c2a9598f2e
Danny Hawkins <danny@quiqup.com>
2024-12-15 15:13:35 +0400

    Demonstrate wire with nested packages

---
+++ wire-di/greeter/greeter.go
@@ -0,0 +1,27 @@
+package greeter
+
+import (
+	"time"
+
+	"github.com/danhawkins/go-examples/wire/message"
+)
+
+type Greeter struct {
+	Grumpy  bool
+	Message message.Message
+}
+
+func (g Greeter) Greet() message.Message {
+	if g.Grumpy {
+		return message.Message("Go away!")
+	}
+	return g.Message
+}
+
+func NewGreeter(m message.Message) Greeter {
+	var grumpy bool
+	if time.Now().Unix()%2 == 0 {
+		grumpy = true
+	}
+	return Greeter{Message: m, Grumpy: grumpy}
+}
--- wire-di/wire.go
+++ wire-di/wire.go
@@ -3,9 +3,14 @@
 
 package main
 
-import "github.com/google/wire"
+import (
+	"github.com/danhawkins/go-examples/wire/event"
+	"github.com/danhawkins/go-examples/wire/greeter"
+	"github.com/danhawkins/go-examples/wire/message"
+	"github.com/google/wire"
+)
 
-func InitializeEvent(phrase string) (Event, error) {
-	wire.Build(NewEvent, NewGreeter, NewMessage)
-	return Event{}, nil
+func InitializeEvent(phrase string) (event.Event, error) {
+	wire.Build(event.NewEvent, greeter.NewGreeter, message.NewMessage)
+	return event.Event{}, nil
 }
